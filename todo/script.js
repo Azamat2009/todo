f5133d58184ca8a8079f1671fc26589447bbc1bf
Azamat <rakhmatov.azamat2009@gmail.com>
2023-02-02 23:38:12 +0500

    updated
    updated

--- todo/script.js
+++ todo/script.js
@@ -8,7 +8,7 @@ let todos = [{
 		id: Math.random(),
 		task: "Do homework",
 		time: new Date().getHours() + ":" + new Date().getMinutes(),
-		completed: false,
+		completed: true,
 	},
 	{
 		id: Math.random(),
@@ -56,18 +56,21 @@ const reload = (arr) => {
 		let krest = document.createElement("img");
 		let h3 = document.createElement("h3");
 		let span = document.createElement("span");
+		let edit = document.createElement('img')
 
+		edit.classList.add('img-2')
 		box.classList.add("box");
 		krest.classList.add("img");
 		h3.classList.add("h3");
 		span.classList.add("span");
 
+		edit.src = "./edit.svg";
 		h3.innerHTML = item.task;
 		krest.src = "./krest.svg";
 		span.innerHTML = item.time;
 
 		container.append(box);
-		box.append(krest,h3,span);
+		box.append(edit,krest,h3,span);
 
 
 		h3.onclick = () => {
@@ -79,16 +82,51 @@ const reload = (arr) => {
 			}
 		}
 		krest.onclick = () => {
-				item.completed = true
-			krest.onclick = () => {
-				box.style.display = 'none'
-			}
+			item.completed = true
+			box.style.display = 'none'
 		}
 		if (item.completed === true) {
 			h3.classList.add('box-active')
 		} else {
 			h3.classList.remove('box-active')
 		}
+
+		edit.onclick = () => {
+
+			let modal = document.createElement("div");
+			let inputTitle = document.createElement("h3")
+			inputTitle.innerHTML = 'Write new todos task'
+			let modalContent = document.createElement("div");
+			let input = document.createElement('input')
+			let close = document.createElement("span");
+			let saveBtn = document.createElement('button')
+			let br = document.createElement('br')
+
+			modal.append(modalContent);
+			document.body.append(modal);
+			modalContent.append(close,inputTitle,input,br,saveBtn);
+
+			modal.style.display = "block";
+			
+			close.innerHTML = "&times;";
+			close.onclick = () => {
+				modal.style.display = "none";
+			};
+
+			saveBtn.innerHTML = "Save Changes";
+			saveBtn.onclick = () => {
+				item.task = input.value;
+				h3.innerHTML = input.value;
+				modal.style.display = "none";
+				reload(todos);
+			};
+
+			modal.classList.add('modal')
+			input.classList.add('md-inp')
+			modalContent.classList.add('modal-content')
+			close.classList.add('close-btn')
+        }
+		
 	};
 }
 reload(todos);
